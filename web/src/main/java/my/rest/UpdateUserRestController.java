3d1885054ec59daf3a0051259fc7848b05cabc1b
Nata Petra <nika.live.1@mail.ru>
2023-01-18 16:34:54 +0300

    Update

--- web/src/main/java/my/rest/UpdateUserRestController.java
+++ web/src/main/java/my/rest/UpdateUserRestController.java
@@ -8,14 +8,14 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@Controller
-public class UpdateUserRestController {
-
-    @Autowired
-    public AdminUserService adminUserService;
-
-    @GetMapping("/admin_update_user/{id}.view")
-    public UserCommonBean updateUserRole(@PathVariable("{user.userId}") Integer id){
-        return adminUserService.showUserById(id);
-    }
-}
+//@Controller
+//public class UpdateUserRestController {
+//
+//    @Autowired
+//    public AdminUserService adminUserService;
+//
+//    @GetMapping("/admin_update_user/{id}.view")
+//    public UserCommonBean updateUserRole(@PathVariable("{user.userId}") Integer id){
+//        return adminUserService.showUserById(id);
+//    }
+//}
